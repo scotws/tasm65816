db08d01bc9b705f3888ab3ba40dac87f903edc9d
Scot W. Stevenson <scot.stevenson@gmail.com>
2015-06-04 01:59:31 +0200

    Added all opcodes to the table, though some are not 8/16-bit ready; additions to manual on extra memory requirements of gforth

--- tasm65816.fs
+++ tasm65816.fs
@@ -2,7 +2,7 @@
 \ Copyright 2015 Scot W. Stevenson <scot.stevenson@gmail.com>
 \ Written with gforth 0.7
 \ First version: 31. May 2015
-\ This version: 02. June 2015
+\ This version: 04. June 2015
 
 \ This program is free software: you can redistribute it and/or modify
 \ it under the terms of the GNU General Public License as published by
@@ -290,7 +290,6 @@ defer a.byte   defer xy.byte
 \ Brute force listing of each possible opcode. Leave undefined entries
 \ empty so it is easier to port this program to other processors
 
-\ TODO modify opcodes for the 65816
 
 \ OPCODES 00 - 0F 
 00 1byte brk       01 2byte ora.dxi   02 2byte cop       03 2byte ora.s
@@ -299,93 +298,93 @@ defer a.byte   defer xy.byte
 0c 3byte tsb       0d 3byte ora       0e 3byte asl       0f 4byte ora.l
 
 \ OPCODES 10 - 1F 
-10 twig bpl        11 2byte ora.ziy   12 2byte ora.zi
-14 2byte trb.z     15 2byte ora.zx    16 2byte asl.zx    17 2byte rmb1.z
-18 1byte clc       19 3byte ora.y     1a 1byte inc.a
-1c 3byte trb       1d 3byte ora.x     1e 3byte asl.x     1f testbranch bbr1
+10 twig bpl        11 2byte ora.diy   12 2byte ora.di    13 2byte ora.siy
+14 2byte trb.d     15 2byte ora.dx    16 2byte asl.dx    17 2byte ora.dily
+18 1byte clc       19 3byte ora.y     1a 1byte inc.a     1b 1byte tcs
+1c 3byte trb       1d 3byte ora.x     1e 3byte asl.x     1f 4byte ora.lx
 
 \ OPCODES 20 - 2F 
-20 3byte jsr       21 2byte and.zxi
-24 2byte bit.z     25 2byte and.z     26 2byte rol.z     27 2byte rmb2.z
-28 1byte plp       29 2byte and.#     2a 1byte rol.a
-2c 3byte bit       2d 3byte and.      2e 3byte rol       2f testbranch bbr2
+20 3byte jsr       21 2byte and.dxi   22 4byte jsr.l     23 2byte and.s
+24 2byte bit.d     25 2byte and.d     26 2byte rol.d     27 2byte and.dil
+28 1byte plp       29 2byte and.#     2a 1byte rol.a     2b 1byte pld
+2c 3byte bit       2d 3byte and.      2e 3byte rol       2f 4byte and.l
 
 \ OPCODES 30 - 3F 
-30 twig bmi        31 2byte and.ziy   32 2byte and.zi
-34 2byte bit.zxi   35 2byte and.zx    36 2byte rol.zx    37 2byte rmb3.z
-38 1byte sec       39 3byte and.y     3a 1byte dec.a
+30 twig bmi        31 2byte and.diy   32 2byte and.di    33 2byte and.siy
+34 2byte bit.dxi   35 2byte and.dx    36 2byte rol.dx    37 2byte and.dily
+38 1byte sec       39 3byte and.y     3a 1byte dec.a     3b 1byte tsc
 3c 3byte bit.x     3d 3byte and.x     3e 3byte rol.x     3f testbranch bbr3
 
 \ OPCODES 40 - 4F 
-40 1byte rti       41 2byte eor.zxi
-                   45 2byte eor.z     46 2byte lsr.z     47 2byte rbm4.z
-48 1byte pha       49 2byte eor.#     4a 1byte lsr.a
-4c 3byte jmp       4d 3byte eor       4e 3byte lsr       4f testbranch bbr4
+40 1byte rti       41 2byte eor.dxi   42 1byte wdm       43 2byte eor.s
+44 3byte mvp       45 2byte eor.d     46 2byte lsr.d     47 2byte eor.dil
+48 1byte pha       49 2byte eor.#     4a 1byte lsr.a     4b 1byte phk
+4c 3byte jmp       4d 3byte eor       4e 3byte lsr       4f 4byte eor.l
 
 \ OPCODES 50 - 5F 
-50 twig bvc        51 2byte eor.ziy   52 2byte eor.zi
-                   55 2byte eor.zx    56 2byte lsr.zx    57 2byte rmb5.z
-58 1byte cli       59 3byte eor.y     5a 1byte phy
-                   5d 3byte eor.x     5e 3byte lsr.x     5f testbranch bbr5
+50 twig bvc        51 2byte eor.diy   52 2byte eor.di    53 2byte eor.siy
+54 3byte mvn       55 2byte eor.dx    56 2byte lsr.dx    57 2byte eor.dily
+58 1byte cli       59 3byte eor.y     5a 1byte phy       5b 1byte tcd
+5c 4byte jmp.l     5d 3byte eor.x     5e 3byte lsr.x     5f 4byte eor.lx
 
 \ OPCODES 60 - 6F 
-60 1byte rts       61 2byte adc.zxi 
-64 2byte stz.z     65 2byte adc.z     66 2byte ror.z     67 2byte rmb6.z
-68 1byte pla       69 2byte adc.#     6a 1byte ror.a
-6c 3byte jmp.i     6d 3byte adc       6e 3byte ror       6f testbranch bbr6
+60 1byte rts       61 2byte adc.dxi   62 3byte per       63 2byte adc.s
+64 2byte stz.d     65 2byte adc.d     66 2byte ror.d     67 2byte adc.dil
+68 1byte pla       69 2byte adc.#     6a 1byte ror.a     6b 1byte rts.l
+6c 3byte jmp.i     6d 3byte adc       6e 3byte ror       6f 4byte adc.l
 
 \ OPCODES 70 - 7F 
-70 twig bvs        71 2byte adc.ziy   72 2byte adc.zi
-74 2byte stz.zx    75 2byte adc.zx    76 2byte ror.zx    77 2byte rmb7.z
-78 1byte sei       79 3byte adc.y     7a 1byte ply
-7c 3byte jmp.xi    7d 3byte adc.x     7e 3byte ror.x     7f testbranch bbr7
+70 twig bvs        71 2byte adc.diy   72 2byte adc.di    73 2byte adc.siy
+74 2byte stz.dx    75 2byte adc.dx    76 2byte ror.dx    77 2byte adc.dily
+78 1byte sei       79 3byte adc.y     7a 1byte ply       7b 1byte tdc
+7c 3byte jmp.xi    7d 3byte adc.x     7e 3byte ror.x     7f 4byte adc.lx
 
 \ OPCODES 80 - 8F 
-80 twig bra        81 2byte sta.zxi
-84 2byte sty.z     85 2byte sta.z     86 2byte stx.z     87 2byte smb0.z
-88 1byte dey       89 2byte bit.#     8a 1byte txa
-8c 3byte sty       8d 3byte sta       8e 3byte stx       8f testbranch bbs0
+80 twig bra        81 2byte sta.dxi   82 3byte bra.l     83 2byte sta.s
+84 2byte sty.d     85 2byte sta.d     86 2byte stx.d     87 2byte sta.dil
+88 1byte dey       89 2byte bit.#     8a 1byte txa       8b 1byte phb
+8c 3byte sty       8d 3byte sta       8e 3byte stx       8f 4byte sta.l
 
 \ OPCODES 90 - 9F 
-90 twig bcc        91 2byte sta.ziy   92 2byte sta.zi
-94 2byte sty.zx    95 2byte sta.zx    96 2byte stx.zy    97 2byte smb1.z
-98 1byte tya       99 3byte sta.y     9a 1byte txs
-9c 3byte stz       9d 3byte sta.x     9e 3byte stz.x     9f testbranch bbs1
+90 twig bcc        91 2byte sta.diy   92 2byte sta.di    93 2byte sta.siy
+94 2byte sty.dx    95 2byte sta.dx    96 2byte stx.dy    97 2byte sta.dily
+98 1byte tya       99 3byte sta.y     9a 1byte txs       9b 1byte txy 
+9c 3byte stz       9d 3byte sta.x     9e 3byte stz.x     9f 4byte sta.lx
 
 \ OPCODES A0 - AF 
-0a0 2byte ldy.#   0a1 2byte lda.zxi  0a2 2byte ldx.#
-0a4 2byte ldy.z   0a5 2byte lda.z    0a6 2byte ldx.z    0a7 2byte smb2.z 
-0a8 1byte tay     0a9 2byte lda.#    0aa 1byte tax
-0ac 3byte ldy     0ad 3byte lda      0ae 3byte ldx      0af testbranch bbs2
+0a0 2byte ldy.#   0a1 2byte lda.dxi  0a2 2byte ldx.#    0a3 2byte lda.s
+0a4 2byte ldy.d   0a5 2byte lda.d    0a6 2byte ldx.d    0a7 2byte lda.dil
+0a8 1byte tay     0a9 2byte lda.#    0aa 1byte tax      0ab 1byte plb
+0ac 3byte ldy     0ad 3byte lda      0ae 3byte ldx      0af 4byte lda.l
 
 \ OPCODES B0 - BF 
-0b0 twig bcs      0b1 2byte lda.ziy  0b2 2byte lda.zi
-0b4 2byte ldy.zx  0b5 2byte lda.zx   0b6 2byte ldx.zy   0b7 2byte smb3.z
-0b8 1byte clv     0b9 3byte lda.y    0ba 1byte tsx
-0bc 3byte ldy.x   0bd 3byte lda.x    0be 3byte ldx.y    0bf testbranch bbs3
+0b0 twig bcs      0b1 2byte lda.diy  0b2 2byte lda.di   0b3 2byte lda.siy
+0b4 2byte ldy.dx  0b5 2byte lda.dx   0b6 2byte ldx.dy   0b7 2byte lda.dily
+0b8 1byte clv     0b9 3byte lda.y    0ba 1byte tsx      0bb 1byte tyx
+0bc 3byte ldy.x   0bd 3byte lda.x    0be 3byte ldx.y    0bf 4byte lda.lx
 
 \ OPCODES C0 - CF 
-0c0 2byte cpy.#   0c1 2byte cmp.zxi
-0c4 2byte cpy.z   0c5 2byte cmp.z    0c6 2byte dec.z    0c7 2byte smb4.z
-0c8 1byte iny     0c9 2byte cmp.#    0ca 1byte dex      0cb 1byte stp
-0cc 3byte cpy     0cd 3byte cmp      0ce 3byte dec      0cf testbranch bbs4
+0c0 2byte cpy.#   0c1 2byte cmp.dxi  0c2 1byte rep      0c3 2byte cmp.s
+0c4 2byte cpy.d   0c5 2byte cmp.d    0c6 2byte dec.d    0c7 2byte cmp.dil
+0c8 1byte iny     0c9 2byte cmp.#    0ca 1byte dex      0cb 1byte wai
+0cc 3byte cpy     0cd 3byte cmp      0ce 3byte dec      0cf 4byte cmp.l
 
 \ OPCODES D0 - DF 
-0d0 twig bne      0d1 2byte cmp.ziy  0d2 2byte cmp.zi
-                  0d5 2byte cmp.zx   0d6 2byte dec.zx   0d7 2byte smb5.z
-0d8 1byte cld     0d9 3byte cmp.y    0da 1byte phx      0db 1byte wai
-                  0dd 3byte cmp.x    0de 3byte dec.x    0df testbranch bbs5
+0d0 twig bne      0d1 2byte cmp.diy  0d2 2byte cmp.di   0d3 2byte cmp.siy
+0d4 1byte pei     0d5 2byte cmp.dx   0d6 2byte dec.dx   0d7 2byte cmp.dily
+0d8 1byte cld     0d9 3byte cmp.y    0da 1byte phx      0db 1byte stp 
+0dc 3byte jmp.il  0dd 3byte cmp.x    0de 3byte dec.x    0df 4byte cmp.lx
 
 \ OPCODES E0 - EF 
-0e0 2byte cpx.#   0e1 2byte sbc.zxi
-0e4 2byte cpx.z   0e5 2byte sbc.z    0e6 2byte inc.z    0e7 2byte smb6.z
-0e8 1byte inx     0e9 2byte sbc.#    0ea 1byte nop
-0ec 3byte cpx     0ed 3byte sbc      0ee 3byte inc      0ef testbranch bbs6
+0e0 2byte cpx.#   0e1 2byte sbc.dxi  0e2 2byte sep      0e3 2byte sbc.s
+0e4 2byte cpx.d   0e5 2byte sbc.d    0e6 2byte inc.d    0e7 2byte sbc.dil
+0e8 1byte inx     0e9 2byte sbc.#    0ea 1byte nop      0eb 1byte xba 
+0ec 3byte cpx     0ed 3byte sbc      0ee 3byte inc      0ef 4byte sbc.l
 
 \ OPCODES F0 - FF 
-0f0 twig beq      0f1 2byte sbc.ziy  0f2 2byte sbc.zi
-                  0f5 2byte sbc.zx   0f6 2byte inc.zx   0f7 2byte smb7.z
-0f8 1byte sed     0f9 3byte sbc.y    0fa 1byte plx
-                  0fd 3byte sbc.x    0fe 3byte inc.x    0ff testbranch bbs7
+0f0 twig beq      0f1 2byte sbc.diy  0f2 2byte sbc.di   0f3 2byte sbc.siy
+0f4 3byte pea     0f5 2byte sbc.dx   0f6 2byte inc.dx   0f7 2byte sbc.dily
+0f8 1byte sed     0f9 3byte sbc.y    0fa 1byte plx      0fb 1byte xce
+0fc 3byte jsr.xi  0fd 3byte sbc.x    0fe 3byte inc.x    0ff 4byte sbc.lx
 
 \ END
